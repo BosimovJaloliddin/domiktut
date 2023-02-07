a762a5df7fb093def19bcbbe95dc2b764c6a05b8
unknown <abdusalomov.aa03@gmail.com>
2023-02-07 16:39:05 +0500

    header add

---
+++ house/js/script.js
@@ -0,0 +1,42 @@
+var burger_o = document.getElementsByClassName("burger-open");
+var burger_x = document.getElementsByClassName("burger-close");
+var bur = document.getElementsByClassName("burger");
+burger_o[0].onclick = function () {
+   bur[0].classList.add("burger-active");
+};
+burger_x[0].onclick = function () {
+   bur[0].classList.remove("burger-active");
+};
+
+var search_o = document.getElementsByClassName("serch-open");
+var search_x = document.querySelectorAll(".serch-close");
+var par = document.getElementsByClassName("nav__serch-form");
+search_o[0].onclick = function () {
+   par[0].classList.add("serch-active");
+   document.getElementById("serch__svg-x").style.display = "flex";
+   document.getElementById("serch__svg-serch").style.display = "none";
+};
+for (var i = 0; i < search_x.length; i++) {
+   search_x[i].onclick = function () {
+      par[0].classList.remove("serch-active");
+      document.getElementById("serch__svg-x").style.display = "none";
+      document.getElementById("serch__svg-serch").style.display = "flex";
+   };
+}
+
+
+var phone_o = document.getElementsByClassName("phone-open");
+var phone_x = document.querySelectorAll(".phone-close");
+var phone = document.getElementsByClassName("phone");
+phone_o[0].onclick = function () {
+   phone[0].classList.add("phone-active");
+   document.getElementById("phone__svg-x").style.display = "flex";
+   document.getElementById("phone__svg-phone").style.display = "none";
+};
+for (var i = 0; i < phone_x.length; i++) {
+   phone_x[i].onclick = function () {
+      phone[0].classList.remove("phone-active");
+      document.getElementById("phone__svg-x").style.display = "none";
+      document.getElementById("phone__svg-phone").style.display = "flex";
+   };
+}
